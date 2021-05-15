f3d0f1a0e2f10a63c4941333d35c21f74b31ac05
jenilSavani155 <jenilsavani155@gmail.com>
2021-05-15 19:00:25 +0530

    commit

--- complaint.sql
+++ complaint.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 14, 2021 at 07:37 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 7.3.28
+-- Generation Time: May 15, 2021 at 03:29 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -40,7 +40,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `username`, `fullName`, `password`, `image`) VALUES
-(1, 'Admin', 'savan makvana', 'e10adc3949ba59abbe56e057f20f883e', '');
+(1, 'Admin', 'savan makvana', '202cb962ac59075b964b07152d234b70', '');
 
 -- --------------------------------------------------------
 
@@ -85,7 +85,9 @@ CREATE TABLE `cloud_engineear` (
 --
 
 INSERT INTO `cloud_engineear` (`id`, `first_name`, `last_name`, `complaint_number`, `status`, `email`, `password`) VALUES
-(5, 'savan', 'makvana', 11, 'on_work', 'savan@gmail.com', 'e10adc3949ba59abbe56e057f20f883e');
+(5, 'savan', 'makvana', 14, 'on_work', 'savan@gmail.com', 'e10adc3949ba59abbe56e057f20f883e'),
+(6, 'engg 1', '', 16, 'on_work', 'engg1@aa.com', '202cb962ac59075b964b07152d234b70'),
+(7, 'eng2', '', 0, '', 'e@a.com', '827ccb0eea8a706c4c34a16891f84e7b');
 
 -- --------------------------------------------------------
 
@@ -98,6 +100,7 @@ CREATE TABLE `complaintremark` (
   `complaintNo` int(55) NOT NULL,
   `status` varchar(255) NOT NULL,
   `remark` varchar(255) NOT NULL,
+  `user_remark` varchar(255) DEFAULT NULL,
   `remarkDate` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -105,24 +108,17 @@ CREATE TABLE `complaintremark` (
 -- Dumping data for table `complaintremark`
 --
 
-INSERT INTO `complaintremark` (`id`, `complaintNo`, `status`, `remark`, `remarkDate`) VALUES
-(1, 3, 'in process', 'your complaint is being processed', '2020-01-13 06:06:21'),
-(2, 3, 'in process', 'your request is in process. kindly have patience', '2020-01-14 07:21:59'),
-(3, 3, 'closed', 'Done', '2020-01-14 07:26:59'),
-(4, 2, 'in process', 'your complaint is being processed..', '2020-01-14 07:29:26'),
-(5, 2, 'closed', 'Your complaint has been resolved', '2020-01-14 07:30:40'),
-(6, 4, 'in process', 'wait...', '2020-01-14 07:31:58'),
-(7, 4, 'closed', 'Request done', '2020-01-14 07:39:24'),
-(8, 6, 'closed', 'Request processed successfully...', '2020-01-14 07:41:46'),
-(9, 6, 'in process', 'in process', '2020-01-14 07:43:06'),
-(10, 7, 'in process', 'request is in pending', '2020-01-14 07:45:56'),
-(11, 5, 'in process', 'complaint is in process ', '2020-01-14 07:55:58'),
-(12, 8, 'in process', 'work is being done over your complaint', '2020-01-14 15:36:32'),
-(13, 8, 'closed', 'Your issue has been resolved', '2020-01-14 15:38:04'),
-(14, 9, 'in process', 'in process, wait for awhile', '2020-01-15 05:08:29'),
-(15, 1, 'closed', 'tteeetetetet', '2021-05-14 17:14:17'),
-(16, 11, 'in process', 'test', '2021-05-14 17:25:56'),
-(17, 11, 'closed', 'close by savan ', '2021-05-14 17:33:44');
+INSERT INTO `complaintremark` (`id`, `complaintNo`, `status`, `remark`, `user_remark`, `remarkDate`) VALUES
+(18, 12, 'in process', 'hello your precedfvvdfd \r\nvf\r\n\r\ndfvd\r\nfvd\r\nfvsddsysdudvhi sdvdfvdfv', 'effrferf', '2021-05-15 06:04:27'),
+(19, 12, 'in process', 'dsvfdvdfsdvsdv\r\nsdvv\r\ndf\r\nvdf\r\nv\r\ndfvd', 'referferfefef', '2021-05-15 06:04:35'),
+(20, 14, 'in process', 'frerferfhttp://localhost/complaint_mgt_sys-master/user/index.php#http://localhost/complaint_mgt_sys-master/user/index.php#http://localhost/complaint_mgt_sys-master/user/index.php#http://localhost/complaint_mgt_sys-master/user/index.php#', 'gtrrrrrrrrrrrrgtrrrrrrrrrrrrgtrrrrrrrrrrrrgtrrrrrrrrrrrr', '2021-05-15 06:15:41'),
+(21, 15, 'in process', 'heihfirgjrighjiv jivfvifjvifvjfvi fijvfivjfivjviv jij vifjv', 'heihfirgjrighjiv jivfvifjvifvjfvi fijvfivjfivjviv jij vifjv', '2021-05-19 06:26:31'),
+(22, 15, 'in process', '22222222\r\nfdvdfv\r\nvdf\r\nvdf\r\nv\r\ndfv\r\ndf\r\nv\r\ndv\r\ndf\r\nvd\r\nv\r\nv', 'heihfirgjrighjiv jivfvifjvifvjfvi fijvfivjfivjviv jij vifjv', '2021-05-20 06:26:31'),
+(23, 13, 'in process', 'processss............', 'hrfhurfhrufrhfur hrfuhfuru rufhrfuhrf', '2021-05-15 08:40:05'),
+(24, 14, '', '', 'replyyyyyyyyyyyyyyyyyyyyyy from DB', '2021-05-15 13:00:46'),
+(25, 14, '', '', 'fffff', '2021-05-15 13:24:25'),
+(26, 14, '', '', 'reply success fully ', '2021-05-15 13:25:04'),
+(27, 14, '', '', 'rfergtrgtrtrtrtrrtttttttttttttttttt', '2021-05-15 13:25:49');
 
 -- --------------------------------------------------------
 
@@ -158,7 +154,12 @@ INSERT INTO `complaints` (`complaintNo`, `user_id`, `cloud_eng_id`, `category_id
 (6, 2, NULL, 4, 'BBA', 'Point Bus issue', 'Complaint', 'we are having issue with the point bus', 'Capture.PNG', '2020-01-13 17:09:00', 'in process', '2020-01-14 07:43:06'),
 (7, 2, NULL, 4, 'Commerce', 'general query Title', 'General Query', 'this is the detail of the complaint', 'N/A', '2020-01-14 07:44:33', 'in process', '2020-01-14 07:45:56'),
 (8, 2, 5, 4, 'Commerce', 'No teacher since 4 days', 'Complaint', 'there is no teacher here in our commerce department', 'N/A', '2020-01-14 15:34:38', 'closed', '2021-05-14 17:01:05'),
-(11, 5, 5, 3, 'Computer Science', 'test complain', 'Complaint', 'testasdfa dfasdfasdfasdf', 'N/A', '2021-05-11 15:48:58', 'closed', '2021-05-14 17:33:44');
+(11, 5, 5, 3, 'Computer Science', 'test complain', 'Complaint', 'testasdfa dfasdfasdfasdf', 'N/A', '2021-05-11 15:48:58', 'closed', '2021-05-14 17:33:44'),
+(12, 6, NULL, 1, 'Computer Science', 'Web Development | Graphic Design | Digital Marketing', 'Complaint', 'Web Development | Graphic Design | Digital MarketingWeb Development | Graphic Design | Digital Marketing', '434458675.png', '2021-05-14 19:31:47', 'in process', '2021-05-15 06:04:27'),
+(13, 6, 6, 3, 'BBA', 'e-commerce with admin panel', 'General Query', 'e-commerce with admin panele-commerce with admin panele-commerce with admin panel', 'Data-Management.png', '2021-05-14 19:31:58', 'in process', '2021-05-15 08:40:05'),
+(14, 6, NULL, 4, 'English', 'Formal Shirt', 'General Query', 'Formal ShirtFormal Shirt Formal ShirtFormal ShirtFormal ShirtFormal ShirtFormal ShirtFormal Shirt', 'N/A', '2021-05-15 06:12:06', 'in process', '2021-05-15 06:15:41'),
+(15, 6, 6, 1, 'Computer Science', 'Business Website', 'Complaint', 'Business WebsiteBusiness WebsiteBusiness WebsiteBusiness WebsiteBusiness WebsiteBusiness WebsiteBusiness WebsiteBusiness WebsiteBusiness WebsiteBusiness Website', 'N/A', '2021-05-15 06:24:09', NULL, '2021-05-15 06:25:30'),
+(16, 6, 6, 3, 'Commerce', 'Web Development | Graphic Design | Digital Marketing', 'Complaint', 'Web Development | Graphic Design | Digital MarketingWeb Development | Graphic Design | Digital MarketingWeb Development | Graphic Design | Digital MarketingWeb Development | Graphic Design | Digital MarketingWeb Development | Graphic Design | Digital MarketingWeb Development | Graphic Design | Digital Marketing', 'N/A', '2021-05-15 06:52:40', NULL, '2021-05-15 06:53:16');
 
 -- --------------------------------------------------------
 
@@ -208,7 +209,8 @@ INSERT INTO `users` (`uid`, `fullName`, `email`, `password`, `department`, `prof
 (1, 'Ansari', 'abdulaziz@gmail.com', 'd861e207ca85f7e171d3225d4a4536cd', '', '', '2020-01-02 13:02:27.441476', '0000-00-00 00:00:00.000000'),
 (2, 'Shahid Raza', 'shahid@gmail.com', 'f3224d90c778d5e456b49c75f85dd668', '', '', '2020-01-03 14:14:58.615533', '0000-00-00 00:00:00.000000'),
 (3, 'Ali Raza', 'ali@gmail.com', '86318e52f5ed4801abe1d13d509443de', '', '', '2020-01-14 15:45:07.011276', '0000-00-00 00:00:00.000000'),
-(5, 'savan', 'savan@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '', '', '2021-05-11 15:47:39.846316', '0000-00-00 00:00:00.000000');
+(5, 'savan', 'savan@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '', '', '2021-05-11 15:47:39.846316', '0000-00-00 00:00:00.000000'),
+(6, 'user', 'user@aa.com', '202cb962ac59075b964b07152d234b70', '', '', '2021-05-11 15:47:39.846316', '0000-00-00 00:00:00.000000');
 
 --
 -- Indexes for dumped tables
@@ -276,19 +278,19 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT for table `cloud_engineear`
 --
 ALTER TABLE `cloud_engineear`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `complaintremark`
 --
 ALTER TABLE `complaintremark`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `complaints`
 --
 ALTER TABLE `complaints`
-  MODIFY `complaintNo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `complaintNo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `department`
@@ -300,7 +302,7 @@ ALTER TABLE `department`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
