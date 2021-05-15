fddc93eac6c739ef527a8ecd0cafff06848b6965
sanjeetsingh77 <sanjeetkooner7755@gmail.com>
2021-05-15 11:19:36 +0530

    commit

--- admin/include/complaint_detail.php
+++ admin/include/complaint_detail.php
@@ -1,4 +1,14 @@
+<?php
 
+if(isset($_GET['cid'])): 
+	$id = $_GET['cid'];
+	$query = "SELECT * FROM complaints WHERE complaintNo=$id";
+	$complaints = $complaint->get_complaints_by_uid($query);
+	foreach($complaints as $cmp){
+//		$cmp->complaintNo;
+	}
+	
+?>
 <h4>Complaint Details: </h4>
 <table class="table table-bordered table-sm" style="padding:4px!important;">
 	<tr>
